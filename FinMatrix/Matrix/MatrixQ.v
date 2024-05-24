abfb2e839f62cf847d23d9cf00f465c479754050
ZhengPu Shi <zhengpushi@nuaa.edu.cn>
2024-05-24 09:58:12 +0800

    update inverse for Q in MatrixQ; update html document

--- FinMatrix/Matrix/MatrixQ.v
+++ FinMatrix/Matrix/MatrixQ.v
@@ -10,6 +10,7 @@
 
 Require Export QExt.
 Require Export MatrixModule.
+Require MatrixQc.
 
 
 (* ######################################################################### *)
@@ -24,6 +25,36 @@ Open Scope mat_scope.
 
 (* ######################################################################### *)
 (** * Matrix theory applied to this type *)
+
+(** Inverse matrix over Q type *)
+(* Although Q type is not supported directly for inverse algorithms (due to
+   the current field structure need Leibniz equal but Q is Setoid equal),
+   We can utilize the ability of Qc type. We can do it by folloing steps:
+   1. use Q2Qc to package input data of Q type to Qc type
+   2. solve the problem with Qc type
+   3. use Qc2Q to unpackage output data of Qc type to Q type.
+*)
+Section inv_Q.
+
+  Import MatrixQc.
+
+  (** Check matrix invertibility with lists as input by GE *)
+  Definition minvtblebListGE (n : nat) (dl : dlist Q) : bool :=
+    minvtblebListGE n (Q2Qc_dlist dl).
+
+  (** Inverse matrix with lists for input and output by GE *)
+  Definition minvListGE (n : nat) (dl : dlist Q) : dlist Q :=
+    Qc2Q_dlist (minvListGE n (Q2Qc_dlist dl)).
+
+  (** Check matrix invertibility with lists as input by AM *)
+  Definition minvtblebListAM (n : nat) (dl : dlist Q) : bool :=
+    minvtblebListAM n (Q2Qc_dlist dl).
+
+  (** Inverse matrix with lists for input and output by AM *)
+  Definition minvListAM (n : nat) (dl : dlist Q) : dlist Q :=
+    Qc2Q_dlist (minvListAM n (Q2Qc_dlist dl)).
+  
+End inv_Q.
 
 
 (* ######################################################################### *)
